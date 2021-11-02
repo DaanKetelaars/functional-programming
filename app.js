16797ffd7d3071f4128c4462038bc9327a61ebfb
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-02 22:19:43 +0100

    Code, cleaned and fully finished

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// Process:
+// 1. import data
+// 2. call data
+// 3. clean data
+// 4. Count & Sort data
+// 5. Write results to the DOM
+
 // Call json with an async import
 // Asnyc is more usable when working with json, more info here: https://github.com/tc39/proposal-json-modules
 const { default: data } = await import("./data.json", {
@@ -44,6 +51,8 @@ function cleanData(dataSet) {
   });
   countData(stripData);
 }
+
+// Count data, based on amount of equal values
 function countData(stripData) {
   let ocr = stripData.reduce(function (acc, curr) {
     return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
@@ -51,12 +60,14 @@ function countData(stripData) {
   refactorData(ocr);
 }
 
+// String the Javascript Object from above and parse it to make an Array out of it
 function refactorData(ocr) {
   const stringData = JSON.stringify(ocr);
   const parseData = JSON.parse(stringData);
   sortData(parseData);
 }
 
+// Sort the data, based on the highest amount of value to the lowest
 function sortData(parseData) {
   let sorting = Object.values(parseData);
   sorting.sort(function (a, b) {
@@ -65,6 +76,7 @@ function sortData(parseData) {
   WriteToDOM(sorting);
 }
 
+// Write all the results to the DOM
 function WriteToDOM(sorting) {
   function makeUL(array) {
     let list = document.createElement("ul");
@@ -78,68 +90,4 @@ function WriteToDOM(sorting) {
   document.getElementById("app").appendChild(makeUL(sorting));
 }
 
-// Add the contents of options[0] to #foo:
-
-// function mapArr(sorting) {
-//   let mappedInArr = Object.entries(sorting).reduce(
-//     (obj, [_key, _value]) => Object.assign(obj, { [_key]: String(_value) }),
-//     {}
-//   );
-// }
-// // const merged = [].concat.apply([], mapArr);
-
-// function makeUL(array) {
-//   const list = document.createElement("ul");
-
-//   for (let i = 0; i < array.length; i++) {
-//     const item = document.createElement("li");
-//     item.appendChild(document.createTextNode(array[i]));
-//     list.appendChild(item);
-//   }
-//   return list;
-// }
-// document.getElementById("app").appendChild(makeUL(mapArr[0]));
-
-// // console.log(OrganizeData);
-// console.log(JSON.stringify(mapArr, null, "\t"));
-
 getData();
-
-// console.log(ParseData);
-// let SortData = Object.entries(ParseData);
-// SortData.sort(function (a, b) {
-//   return b - a;
-// });
-// console.log(occurrences);
-// const result = Object.entries(occurrences);
-
-// let arr = [];
-// arr.push(ParseData);
-// console.log(arr);
-
-// if (typeof arr === "object") {
-//   console.log("hello");
-// } else {
-//   console.log("niet");
-// }
-
-// if (typeof arr === "object" && arr !== null) {
-//   console.log("hello");
-// }
-
-// Printing values
-// for (var i = 0; i < result.length; i++) {
-//   for (var z = 0; z < result[i].length; z++) {
-//     document.write(result[i][z] + " ");
-//   }
-//   document.write("</br>");
-// }
-// console.log(result);
-
-// console.log(SortData);
-// console.log(JSON.stringify(ParseData, null, "\t"));
-
-// let test = Object.keys(SortData);
-// let testTwee = Object.keys(ParseData);
-// console.log(testTwee);
-// console.log(test);

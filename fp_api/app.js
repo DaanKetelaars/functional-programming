20fa9cca7915dc69bd2d169451bdc5f35d8a7ace
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-05 00:46:00 +0100

    update 03

--- fp_api/app.js
+++ fp_api/app.js
@@ -2,21 +2,20 @@
 const axios = require("axios").default;
 require("dotenv").config();
 
-
-let arr = [];
-
+// async await function om de data op te roepen en te filteren
 async function getApi() {
     await axios(process.env.API_URL)
         .then(function (res) {
             let getData = res.data.topartists.artist;
             let showArtists = getData.map((data) => data);
-            showArtists.forEach(artist => {
+            showArtists.map(artist => {
                 const listeners = artist.listeners;
                 cleanData(listeners)
             });
         });
 }
 
+// Values omzetten naar numbers en dan LocaleString om decimalen toe te voegen. Direct daarna zet ik hem weer terug naar numbers
 function cleanData(listeners) {
     let StrToNum = parseFloat(listeners).toLocaleString('de-DE');
     let cleanNum = parseFloat(StrToNum);
@@ -25,7 +24,7 @@ function cleanData(listeners) {
 }
 
 
-
+// Uiteindelijk hier niet aan toe gekomen.
 function sortData(cleanNum) {
     // sorteren met .sort()
 }

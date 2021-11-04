57033b66724388650129e644dc97883447b51fd4
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-04 23:40:45 +0100

    update 02

--- fp_api/app.js
+++ fp_api/app.js
@@ -3,28 +3,21 @@ const axios = require("axios").default;
 require("dotenv").config();
 
 
-
 let artists = [];
-// async function getApi() {
-//     await axios(process.env.API_URL)
-//     try {
-//         (function (res) {
-//             let getData = res.data.topartists.artist;
-//             let showArtists = getData.map((data) => data);
-//             showArtists.forEach(artist => {
-//                 let listeners = artist.listeners;
-//                 cleanData(listeners);
-//                 console.log(listeners);
-//             });
-//         });
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
-function cleanData(listeners) {
-    console.log(Math.abs(Number(listeners)) >= 1.0e+6);
+async function getApi() {
+    await axios(process.env.API_URL)
+        .then(function (res) {
+            let getData = res.data.topartists.artist;
+            let showArtists = getData.map((data) => data);
+            showArtists.forEach(artist => {
+                let listeners = artist.listeners;
+                cleanData(listeners);
+                console.log(listeners);
+            });
+        });
 }
+
+function cleanData(listeners) {}
 
 
 getApi();

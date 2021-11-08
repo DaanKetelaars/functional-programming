b31dcae6580cd59fcc005661c64779864cab9f6d
Daan Ketelaars <daanketelaars@gmail.com>
2021-11-08 11:25:25 +0100

    update sorting

--- fp_api/app.js
+++ fp_api/app.js
@@ -2,6 +2,8 @@
 const axios = require("axios").default;
 require("dotenv").config();
 
+
+let listenersArr = []
 // async await function om de data op te roepen en te filteren
 async function getApi() {
     await axios(process.env.API_KEY)
@@ -10,25 +12,21 @@ async function getApi() {
             let showArtists = getData.map((data) => data);
             showArtists.map(artist => {
                 const listeners = artist.listeners;
-                cleanData(listeners)
+                if (typeof listeners === 'string') {
+                    listenersArr.push(listeners);
+                }
             });
+            cleanData(listenersArr);
         });
 }
 
-// Values omzetten naar numbers en dan LocaleString om decimalen toe te voegen. Direct daarna zet ik hem weer terug naar numbers
-function cleanData(listeners) {
-    let StrToNum = parseFloat(listeners).toLocaleString('de-DE');
-    let cleanNum = parseFloat(StrToNum);
-    console.log(cleanNum);
-    sortData(cleanNum);
+// Values omzetten naar numbers en dan LocaleString om decimalen toe te voegen.Direct daarna zet ik hem weer terug naar numbers
+function cleanData(listenersArr) {
+    let newListenersArr = listenersArr;
+    let StrToNum = newListenersArr.map((i) => Number(i));
+    StrToNum = StrToNum.sort((a, b) => b - a);
+    console.log(StrToNum);
+    StrToNum = Object.values(StrToNum);
 }
-
-
-// Uiteindelijk hier niet aan toe gekomen.
-function sortData(cleanNum) {
-    // sorteren met .sort()
-}
-
-
 
 getApi();
